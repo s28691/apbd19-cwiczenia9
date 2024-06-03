dc310a415a40ffa0b95e6b6b2c069b74a313559a
s28691 <s28691@pjwstk.edu.pl>
2024-06-03 23:37:13 +0200

    Adding POST and DELETE endpoints

--- apbd19-cwiczenia9/apbd19-cwiczenia9/Controllers/TripsController.cs
+++ apbd19-cwiczenia9/apbd19-cwiczenia9/Controllers/TripsController.cs
@@ -1,4 +1,6 @@
 ﻿using apbd19_cwiczenia9.Data;
+using apbd19_cwiczenia9.DTO;
+using apbd19_cwiczenia9.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -36,5 +38,62 @@ public class TripsController : ControllerBase
             })
         }));
     } 
+    [HttpDelete("{idClient}")]
+    public async Task<IActionResult> DeleteClient(int idClient)
+    {
+        var client = await _context.Clients
+            .Include(client => client.ClientTrips)
+            .FirstOrDefaultAsync(client => client.IdClient == idClient);
+        if (client == null)
+        {
+            return NotFound("Client not found");
+        }
+        if (client.ClientTrips.Any())
+        {
+            return BadRequest("Cannot delete client with connected trips");
+        }
+        _context.Clients.Remove(client);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
     
+    [HttpPost("{idTrip}/clients")]
+    public async Task<IActionResult> AssignClientToTrip(int idTrip, [FromBody] ClientTripDTO request)
+    {
+        var existingClient = await _context.Clients.FirstOrDefaultAsync(client => client.Pesel == request.Pesel);
+        if (existingClient != null)
+        {
+            return BadRequest("Client with this PESEL exists!");
+        }
+        var trip = await _context.Trips.FirstOrDefaultAsync(trip => trip.IdTrip == idTrip);
+        if (trip == null)
+        {
+            return NotFound("Trip not found!");
+        }
+        if (trip.DateFrom <= DateTime.Now)
+        {
+            return BadRequest("Trip already started!");
+        }
+        var newClient = new Client
+        {
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            Email = request.Email,
+            Telephone = request.Telephone,
+            Pesel = request.Pesel
+        };
+        _context.Clients.Add(newClient);
+        await _context.SaveChangesAsync();
+
+        var clientTrip = new ClientTrip
+        {
+            IdClient = newClient.IdClient,
+            IdTrip = idTrip,
+            RegisteredAt = DateTime.Now,
+            PaymentDate = request.PaymentDate
+        };
+        _context.ClientTrips.Add(clientTrip);
+        await _context.SaveChangesAsync();
+        return Ok();
+    }
 }
---
+++ apbd19-cwiczenia9/apbd19-cwiczenia9/DTO/ClientTripDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace apbd19_cwiczenia9.DTO;
+
+public class ClientTripDTO
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Telephone { get; set; } = null!;
+    public string Pesel { get; set; } = null!;
+    public DateTime? PaymentDate { get; set; }
+}
