0c03a2e863819f7c19a462cb8741eefbc404da04
s28691 <s28691@pjwstk.edu.pl>
2024-06-03 23:11:41 +0200

    Creating context and first task

---
+++ apbd19-cwiczenia9/apbd19-cwiczenia9/Controllers/TripsController.cs
@@ -0,0 +1,40 @@
+﻿using apbd19_cwiczenia9.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace apbd19_cwiczenia9.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+public class TripsController : ControllerBase
+{
+    private readonly ApbdContext _context;
+
+    public TripsController(ApbdContext context)
+    {
+        _context = context;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetTrips()
+    {
+        var trips = await _context.Trips.Select(e => new
+        {
+            Name = e.Name,
+            Countries = e.IdCountries.Select(c => new {
+                Name = c.Name
+            })
+        }).ToListAsync();
+        var TripsInclude = await _context.Trips
+            .Include(e => e.IdCountries)
+            .ToListAsync();
+        return Ok(TripsInclude.Select(e => new
+        {
+            Name = e.Name,
+            Country = e.IdCountries.Select(c => new
+            {
+                Name = c.Name
+            })
+        }));
+    } 
+    
+}
--- apbd19-cwiczenia9/apbd19-cwiczenia9/Program.cs
+++ apbd19-cwiczenia9/apbd19-cwiczenia9/Program.cs
@@ -1,9 +1,13 @@
+using apbd19_cwiczenia9.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<ApbdContext>(options => options.UseSqlServer("Name=ConnectionStrings:Default"));
 
 var app = builder.Build();
 
@@ -13,7 +17,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.MapControllers();
 app.UseHttpsRedirection();
 
 
